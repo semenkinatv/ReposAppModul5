02bb81895eda069880e9f8b7136c8418ca234ab2
semenkinatv <semenkinatv@gmail.com>
2021-03-05 01:34:35 +0300

    Метод

--- ConsoleAppModul5/Program.cs
+++ ConsoleAppModul5/Program.cs
@@ -4,20 +4,71 @@ namespace ConsoleAppModul5
 {
     class Program
     {
-        static void Main(string[] args)
+        static void ShowColor()
+    {
+        Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+        var color = Console.ReadLine();
+
+        switch (color)
+        {
+            case "red":
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Your color is red!");
+                break;
+
+            case "green":
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Your color is green!");
+                break;
+            case "cyan":
+                Console.BackgroundColor = ConsoleColor.Cyan;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Your color is cyan!");
+                break;
+            default:
+                Console.BackgroundColor = ConsoleColor.Yellow;
+                Console.ForegroundColor = ConsoleColor.Red;
+
+                Console.WriteLine("Your color is yellow!");
+                break;
+        }
+    }
+
+        public static void Main(string[] args)
         {
             (string Name, string[] Dishes) User;
             Console.WriteLine("Введите имя пользователя:");
             User.Name = Console.ReadLine();
 
             User.Dishes = new string[5];
-;            for (int i = 0; i < 5; i++)
+            ; for (int i = 0; i < 5; i++)
             {
                 Console.WriteLine("Введите любимое блюдо № {0}", i + 1);
                 User.Dishes[i] = Console.ReadLine();
-             }
+            }
 
             Console.WriteLine("Ваше любимое блюдо №3 {0}", User.Dishes[2]);
+
+            
+            var (name, age) = ("Евгения", 27);
+
+            Console.WriteLine("Мое имя: {0}", name);
+            Console.WriteLine("Мой возраст: {0}", age);
+
+            Console.Write("Введите имя: ");
+            name = Console.ReadLine();
+            Console.Write("Введите возраст с цифрами:");
+            age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Ваше имя: {0}", name);
+            Console.WriteLine("Ваш возраст: {0}", age);
+
+            ShowColor();
 
             Console.ReadKey();
         }
