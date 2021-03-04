6769a981ac4f3000fdb485fa3076938da2e9a0dc
semenkinatv <semenkinatv@gmail.com>
2021-03-05 01:08:50 +0300

    Цикл

--- ConsoleAppModul5/Program.cs
+++ ConsoleAppModul5/Program.cs
@@ -6,7 +6,20 @@ namespace ConsoleAppModul5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            (string Name, string[] Dishes) User;
+            Console.WriteLine("Введите имя пользователя:");
+            User.Name = Console.ReadLine();
+
+            User.Dishes = new string[5];
+;            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Введите любимое блюдо № {0}", i + 1);
+                User.Dishes[i] = Console.ReadLine();
+             }
+
+            Console.WriteLine("Ваше любимое блюдо №3 {0}", User.Dishes[2]);
+
+            Console.ReadKey();
         }
     }
 }
